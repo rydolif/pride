88a6b9ff714ef0d98d39328e6e8681e745ab28a4
rudolif <rudolifrudolif@gmail.com>
2019-01-23 11:06:35 +0200

    commit pravki2

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2528,6 +2528,20 @@ $(function() {
       $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
   });
   
+//-------------------------скорость якоря---------------------------------------
+  $(".nav ul").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
+  //--------------------закриття меню при кліку на ссилку якоря--------------------
+     $('.hamburger').removeClass('hamburger--active');
+     $('.header-menu').removeClass('header-menu');
+     $('.header--active').removeClass('header--active');
+     $('.nav--active').removeClass('nav--active');
+
+  });
+
 });
 
 //----------------------------------------preloader----------------------------------
